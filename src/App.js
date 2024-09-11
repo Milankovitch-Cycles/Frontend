f686b5a125f7d235bbacef8273f2c04f26d67e6a
Cbravor1991 <cbravor@fi.uba.ar>
2024-09-11 12:32:43 -0300

    feat: add login and register forms

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Suspense } from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
+// Lazy load del componente Register
+const Register = React.lazy(() => import('./Pages/Register/Register'));
+const Login = React.lazy(() => import('./Pages/Login/Login'));
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+        <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </Suspense>
+      </BrowserRouter>
   );
 }
 
