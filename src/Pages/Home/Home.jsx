c996dd85e7c4ebe5ae2c7909eda23ee86bc55cfd
gmovia <gmovia@fi.uba.ar>
2024-10-25 20:19:49 -0300

    feat: refactor sidebar

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,32 +1,10 @@
 import React, { useState } from "react";
 import Sidebar from "../Sidebar/Sidebar";
-import ListWells from "../ListWells/ListWells"; 
+import ListWells from "../ListWells/ListWells";
 import LoadWell from "../LoadWell/LoadWell";
 
 const Home = () => {
-  const [currentComponent, setCurrentComponent] = useState(<ListWells />); 
-
-  const handleNavigation = (segment) => {
-    console.log("Navegando a:", segment);
-    switch (segment) {
-      case "cargarPozos":
-        console.log("hola mundo");
-        setCurrentComponent(<LoadWell />);
-        break;
-      case "listarPozos":
-        setCurrentComponent(<ListWells />);
-        break;
-      default:
-        setCurrentComponent(<ListWells />);
-        break;
-    }
-  };
-
-  return (
-    <Sidebar onNavigate={handleNavigation}>
-      {currentComponent} 
-    </Sidebar>
-  );
+  return <Sidebar></Sidebar>;
 };
 
 export default Home;
--- src/Pages/Sidebar/Sidebar.jsx
+++ src/Pages/Sidebar/Sidebar.jsx
@@ -1,13 +1,13 @@
 import * as React from "react";
+import PropTypes from "prop-types";
 import { extendTheme } from "@mui/material/styles";
 import DashboardIcon from "@mui/icons-material/Dashboard";
-import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
-import BarChartIcon from "@mui/icons-material/BarChart";
-import DescriptionIcon from "@mui/icons-material/Description";
-import LayersIcon from "@mui/icons-material/Layers";
+import TimelineIcon from "@mui/icons-material/Timeline";
 import { AppProvider } from "@toolpad/core/AppProvider";
 import { DashboardLayout } from "@toolpad/core/DashboardLayout";
-import { Chip, Button } from "@mui/material"; 
+import { useDemoRouter } from "@toolpad/core/internal";
+import LoadWell from "../LoadWell/LoadWell";
+import ListWells from "../ListWells/ListWells";
 
 const NAVIGATION = [
   {
@@ -15,63 +15,14 @@ const NAVIGATION = [
     title: "Main items",
   },
   {
-    segment: "listarPozos",
-    title: "Listar Pozos",
+    segment: "loadWell",
+    title: "Page",
     icon: <DashboardIcon />,
-    action: (handleNavigation) => (
-      <Button
-        onClick={() => handleNavigation("listarPozos")}
-      >
-        Listar
-      </Button>
-    ),
   },
   {
-    segment: "cargarPozos",
-    title: "Cargar Pozos",
-    icon: <ShoppingCartIcon />,
-    action: (handleNavigation) => (
-      <Button
-        onClick={() => handleNavigation("cargarPozos")}
-       
-      >
-        Cargar
-      </Button>
-    ),
-  },
-  {
-    segment: "/loadWell",
-    title: "Prueba carga pozo",
-    icon: <ShoppingCartIcon />
-  },
-  {
-    kind: "divider",
-  },
-  {
-    kind: "header",
-    title: "Analytics",
-  },
-  {
-    segment: "reports",
-    title: "Reports",
-    icon: <BarChartIcon />,
-    children: [
-      {
-        segment: "sales",
-        title: "Sales",
-        icon: <DescriptionIcon />,
-      },
-      {
-        segment: "traffic",
-        title: "Traffic",
-        icon: <DescriptionIcon />,
-      },
-    ],
-  },
-  {
-    segment: "integrations",
-    title: "Integrations",
-    icon: <LayersIcon />,
+    segment: "listWells",
+    title: "Page 2",
+    icon: <TimelineIcon />,
   },
 ];
 
@@ -89,40 +40,27 @@ const demoTheme = extendTheme({
   },
 });
 
-function useDemoRouter(initialPath) {
-  const [pathname, setPathname] = React.useState(initialPath);
+function DemoPageContent({ pathname }) {
+  if (pathname === "/loadWell") {
+    return <LoadWell />;
+  }
 
-  const router = React.useMemo(() => {
-    return {
-      pathname,
-      searchParams: new URLSearchParams(),
-      navigate: (path) => setPathname(String(path)),
-    };
-  }, [pathname]);
+  if (pathname === "/listWells") {
+    return <ListWells />;
+  }
 
-  return router;
+  return <ListWells></ListWells>; // Agregar mas casos
 }
 
-export default function Sidebar(props) {
-  const { window, children, onNavigate } = props;
+DemoPageContent.propTypes = {
+  pathname: PropTypes.string.isRequired,
+};
+
+function AppProviderTheme(props) {
+  const { window } = props;
 
   const router = useDemoRouter("/dashboard");
-
-  const demoWindow = window ? window() : undefined;
-
-  // Función para manejar la navegación
-  const handleNavigation = (segment) => {
-    console.log("Navigating to:", segment);
-    if (onNavigate) {
-      onNavigate(segment);
-    }
-  };
-
-  // Modifica la navegación para incluir la función handleNavigation
-  const navigationWithHandlers = NAVIGATION.map((item) => ({
-    ...item,
-    action: item.action ? item.action(handleNavigation) : undefined, // Llama a la acción pasando handleNavigation
-  }));
+  const demoWindow = window !== undefined ? window() : undefined;
 
   return (
     <AppProvider
@@ -130,12 +68,20 @@ export default function Sidebar(props) {
         logo: <img src="https://mui.com/static/logo.png" alt="MUI logo" />,
         title: "My App",
       }}
-      navigation={navigationWithHandlers} // Usa la navegación con handlers
+      navigation={NAVIGATION}
       router={router}
       theme={demoTheme}
       window={demoWindow}
     >
-      <DashboardLayout>{children}</DashboardLayout>
+      <DashboardLayout>
+        <DemoPageContent pathname={router.pathname} />
+      </DashboardLayout>
     </AppProvider>
   );
 }
+
+AppProviderTheme.propTypes = {
+  window: PropTypes.func,
+};
+
+export default AppProviderTheme;
