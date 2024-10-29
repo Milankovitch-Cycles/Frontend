dc1fdf32501b07a731b24c171edd9fffecf207fc
gmovia <gmovia@fi.uba.ar>
2024-10-28 21:42:28 -0300

    feat: add icons

--- src/Pages/Sidebar/Sidebar.jsx
+++ src/Pages/Sidebar/Sidebar.jsx
@@ -6,6 +6,9 @@ import { AppProvider } from "@toolpad/core/AppProvider";
 import { DashboardLayout } from "@toolpad/core/DashboardLayout";
 import { useDemoRouter } from "@toolpad/core/internal";
 import ListWells from "../ListWells/ListWells";
+import LogoutIcon from "@mui/icons-material/Logout";
+import PersonIcon from "@mui/icons-material/Person";
+import ArticleIcon from "@mui/icons-material/Article";
 
 const NAVIGATION = [
   {
@@ -16,6 +19,21 @@ const NAVIGATION = [
     segment: "listWells",
     title: "Pozos",
     icon: <TimelineIcon />,
+  },
+  {
+    segment: "listProccess", // TO-DO: Change this
+    title: "Procesamientos",
+    icon: <ArticleIcon />, // TO-DO: Change this
+  },
+  {
+    segment: "user", // TO-DO: Change this
+    title: "Mi cuenta",
+    icon: <PersonIcon />, // TO-DO: Change this
+  },
+  {
+    segment: "signOut", // TO-DO: Change this
+    title: "Cerrar sesión",
+    icon: <LogoutIcon />, // TO-DO: Change this
   },
   // TO-DO: Add sign out
 ];
